5bb72d02d0808c0989c9ae74dd4bd235632eac40
Gabriela Schirmer <gschirmer@furb.br>
2022-05-23 21:50:18 -0300

    mensagem

---
+++ POO/src/classes/HotDog.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class HotDog extends Lanche {
+
+    public HotDog(){
+        this.adicionarIngrediente("Pão");
+        this.adicionarIngrediente("Salsicha");
+        this.adicionarIngrediente("Purê de batata");
+        this.adicionarIngrediente("Batata palha");
+        this.adicionarIngrediente("Vinagrete");
+        this.adicionarIngrediente("Queijo cheddar");
+        this.adicionarIngrediente("Milho");
+        this.adicionarIngrediente("Ervilha");
+    }
+
+}
---
+++ POO/src/classes/MistoQuente.java
@@ -0,0 +1,9 @@
+package classes;
+
+public class MistoQuente extends Lanche {
+    public MistoQuente(){
+        this.adicionarIngrediente("Pão");
+        this.adicionarIngrediente("Preijo");
+        this.adicionarIngrediente("Queijunto");
+    }
+}
